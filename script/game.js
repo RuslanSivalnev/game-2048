2894e1dcf74b0f40c8190af7a194ba484b44d65c
Ruslan <mypo4tt@gmai.com>
2018-12-04 15:45:24 +0200

    add helper function

--- script/game.js
+++ script/game.js
@@ -1,34 +1,54 @@
+let createAndAppend = function({className, parentElement, value}, tag = 'div'){
+    let element = document.createElement(tag);
+    element.className = className;
+    if(value){
+        element.innerHTML = value
+    };
+    parentElement.appendChild(element);
+
+    return element;
+}
+
+
+
+
 class Game {
     //генираци поля по разметке html
 
     constructor(parentElement, { size = 4 }) {
-        let gameFieldElement = document.createElement('div');
-        gameFieldElement.className ='game';
-        parentElement.appendChild(gameFieldElement);
-
-        let headerElement = document.createElement('div');
-        headerElement.className = 'header';
-        gameFieldElement.appendChild(headerElement);
+        let gameFieldElement = createAndAppend({
+            className: 'game',
+            parentElement
+        });
+        // let gameFieldElement = document.createElement('div');
+        // gameFieldElement.className ='game';
+        // parentElement.appendChild(gameFieldElement);
+        let headerElement = createAndAppend({
+            className:'header',
+            parentElement: gameFieldElement
+        })
 
         this.rating = 0;
 
         headerElement.innerHTML = `Rating: ${this.rating}`;
 
-        //создание элемена с калссом field        
-        let fieldElement = document.createElement('div');
-        fieldElement.className = 'field';
-        gameFieldElement.appendChild(fieldElement);
+        let fieldElement = createAndAppend({
+            className: 'field',
+            parentElement: gameFieldElement
+        })      
+     
 
         for (let i = 0; i < size; i++) {
             for (let k = 0; k < size; k++){
-                let cellElement = document.createElement('div');
-                cellElement.className = 'cell';
+                let cellElement = createAndAppend({
+                    className:'cell',
+                    parentElement: fieldElement
+                });
 
                 if(Math.random()>0.8){
                     cellElement.innerHTML = Math.random()>0.5 ? 4 : 2;
 
                 }
-                fieldElement.appendChild(cellElement);
             }
         }
     }
